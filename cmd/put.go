9c489254043933eb6662d2b47ac162688cec0d5f
Peeyush Tyagi <mastermind12210@gmail.com>
2026-03-12 16:48:06 +0530

    fix: Identity put functionality updated

--- cmd/put.go
+++ cmd/put.go
@@ -3,6 +3,7 @@ package cmd
 
 import (
 	"fmt"
+	db "xsh/internal/db"
 	"xsh/internal/identity"
 
 	"github.com/spf13/cobra"
@@ -12,17 +13,24 @@ var putCmd = &cobra.Command{
 	Use:     "put",
 	Short:   "Store data in the database.",
 	Long:    "Store data in the database based on specified criteria.",
-	Args:    cobra.ExactArgs(1),
+	Args:    cobra.ExactArgs(3),
 	RunE:    putData,
 	Example: "xsh put identity",
 }
 
 func putData(cmd *cobra.Command, args []string) error {
-	switch args[0] {
+	dbConnection, err := db.GetDB()
+	if err != nil {
+		return fmt.Errorf("Error connecting to database: %w", err)
+	}
+	defer dbConnection.Close()
+
+	dataType, name, path := args[0], args[1], args[2]
+	switch dataType {
 	case "i":
 		fallthrough
 	case "identity":
-		return identity.PutIdentity()
+		return identity.PutIdentity(dbConnection, name, path)
 	default:
 		return fmt.Errorf("invalid data type selected for inserting")
 	}
--- internal/identity/put.go
+++ internal/identity/put.go
@@ -1,6 +1,11 @@
 package identity
 
-func PutIdentity() error {
-	// id := Identity{}
-	return nil
+import "database/sql"
+
+func PutIdentity(db *sql.DB, name, path string) error {
+	id, err := NewIdentity(name, path)
+	if err != nil {
+		return nil
+	}
+	return id.Store(db)
 }
